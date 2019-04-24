d87a227b3ffd885576b1386100a5d518094d42ef
Dennis Muturia <dmuturia2922@gmail.com>
2019-04-24 15:37:54 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Main {
+    //Sret the queue name that we will be sending the messages
+    private final static String QUEUE_NAME = "testing";
+    public  static void main(String[] args) throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try(Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()){
+            channel.queueDeclare(QUEUE_NAME, false,false,false,null);
+            String message = "Hello World";
+
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+
+            System.out.println("[x] Sent'" + message +"'");
+        }
+    }
+}
